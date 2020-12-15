e150d297c8c3d66890037b55218cd720dbe94ec8
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 16:26:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season|
     puts "#{season[0].capitalize}:"
+
+    season.each do |holiday, supplies|
+      puts "  #{holiday}"
+    end
   end
 end
 
