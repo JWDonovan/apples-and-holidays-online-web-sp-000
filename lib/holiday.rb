10db77a854abfd6fecd94c7cb5409216363eb55a
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 16:22:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season|
-    puts "#{season}:"
+    puts "#{season.to_s}:"
 
     season.each do |holiday, supplies|
       puts "  #{holiday}: #{supplies.join(', ')}"
